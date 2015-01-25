9efe2e4b2a5ce1af2bfa1f806b5e0ea3055b0071
Bernard Mantha <bernard.mantha@gmail.com>
2015-01-25 11:58:51 -0500

    added list one

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
-
 var wordcatApp = angular.module('wordcatApp', ['ngTouch']);
 wordcatApp.controller('WordListCtrl', function ($scope) {
-  $scope.words = [
-    {'name': 'with','list':'1'},
-    {'name': 'cat','list':'1'},
-	{'name': 'dog','list':'1'}
-  ];
-  $scope.index = 0;
-  $scope.word = $scope.words[$scope.index].name;
-  $scope.Next = function(){
-	  console.log('next fired');
-	  if ($scope.index < ($scope.words.length -1)) $scope.index++;
-	    $scope.word = $scope.words[$scope.index].name;
-  }
-  $scope.Previous = function(){
-	  console.log('previous fired');
-	  if ($scope.index>0) $scope.index--;
-	    $scope.word = $scope.words[$scope.index].name;
-  }
+    $scope.words = [{
+        'name': 'the',
+            'list': '1'
+    }, {
+        'name': 'no',
+            'list': '1'
+    }, {
+        'name': 'in',
+            'list': '1'
+    }, {
+        'name': 'is',
+            'list': '1'
+    }, {
+        'name': 'I',
+            'list': '1'
+    }, {
+        'name': 'it',
+            'list': '1'
+    }, {
+        'name': 'go',
+            'list': '1'
+    }, {
+        'name': 'me',
+            'list': '1'
+    }, {
+        'name': 'and',
+            'list': '1'
+    },{
+        'name': 'a',
+            'list': '1'
+    }
+	];
+    $scope.index = 0;
+    $scope.word = $scope.words[$scope.index].name;
+    $scope.Next = function () {
+        console.log('next fired');
+        if ($scope.index < ($scope.words.length - 1)) $scope.index++;
+        $scope.word = $scope.words[$scope.index].name;
+    };
+    $scope.Previous = function () {
+        console.log('previous fired');
+        if ($scope.index > 0) $scope.index--;
+        $scope.word = $scope.words[$scope.index].name;
+    };
 });
 console.log('script ran');
