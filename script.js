e4e443ddac34b1167b9da4b14b1a0dc72df2fd6c
Bernard Mantha <bernard.mantha@gmail.com>
2015-01-25 10:43:03 -0500

    model built

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-(function(angular) {
-  'use strict';
-angular.module('ngSwipeLeftExample', ['ngTouch']);
-})(window.angular);
+
+var wordcatApp = angular.module('wordcatApp', []);
+wordcatApp.controller('WordListCtrl', function ($scope) {
+  $scope.words = [
+    {'name': 'with','list':'1'},
+    {'name': 'cat','list':'1'},
+	{'name': 'dog','list':'1'}
+  ];
+  $scope.index = 0;
+  $scope.word = $scope.words[$scope.index].name;
+  $scope.Next = function(){
+	  if (index < ($scope.words.length -1)) index++;
+  }
+  $scope.Previous = function(){
+	  if (index>0) index--;
+  }
+});
+console.log('script ran');
